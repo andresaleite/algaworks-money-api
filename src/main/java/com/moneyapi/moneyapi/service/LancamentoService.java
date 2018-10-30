b552949f279e44682d75c364872e5062c6b0a385
Andressa Leite Lopes Prates <andresa.allp@pf.gov.br>
2018-10-30 17:50:20 -0300

    aula 7.5 Nome do usuario logado funcionando

--- src/main/java/com/moneyapi/moneyapi/service/LancamentoService.java
+++ src/main/java/com/moneyapi/moneyapi/service/LancamentoService.java
@@ -1,7 +1,6 @@
 package com.moneyapi.moneyapi.service;
 
 import java.io.IOException;
-import java.util.Optional;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
--- src/main/java/com/moneyapi/moneyapi/service/PessoaService.java
+++ src/main/java/com/moneyapi/moneyapi/service/PessoaService.java
@@ -1,7 +1,5 @@
 package com.moneyapi.moneyapi.service;
 
-import java.util.Optional;
-
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
