42f9f6a9fd6fdff30ab0622373c32624bd60b3f7
Andresa Prates <andresaleite@gmail.com>
2019-01-25 23:30:39 -0200

    teste

--- src/main/java/com/moneyapi/moneyapi/config/property/MoneyApiProperty.java
+++ src/main/java/com/moneyapi/moneyapi/config/property/MoneyApiProperty.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties("moneyapi")
 public class MoneyApiProperty {
 
-	private String originPermitida = "https://localhost:8000";
+	private String originPermitida = "https://desa-money-api.herokuapp.com";
 	private final Seguranca seguranca = new Seguranca();
 	public static class Seguranca{
 		private boolean enableHttps; 
